40d14e2f0fe6a7ffe26407860f0f0d3b0338d088
Jonathan Hooley <jjhooley@engr-134-02.eastern.ewu.edu>
2014-05-21 14:19:22 -0700

    menutron version 0

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+
+  	$("nav#main").menutron({
+  		menuTitle: "Snowboard Powder",
+  		maxScreenWidth: "800"
+
+  	});
+
+});
